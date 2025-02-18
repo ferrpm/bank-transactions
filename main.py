23ec7e0fa95ebaab8eb15cbdb536249c67957063
Fer Porras <ferpm09@gmail.com>
2025-02-17 22:52:43 -0600

    Feat: Export transactions to txt file

--- main.py
+++ main.py
@@ -13,46 +13,46 @@ mail.login(EMAIL_USER, EMAIL_PASS)
 mail.select("inbox")
 
 result, data = mail.search(None, '(UNSEEN FROM "notificacion@notificacionesbaccr.com")')
+txt_path = r"C:\Users\Fer\Desktop\transacciones.txt"
 transactions = []
 
-for num in data[0].split():
-    result, msg_data = mail.fetch(num, "(RFC822)")
-    raw_email = msg_data[0][1]
-    msg = email.message_from_bytes(raw_email)
-    email_body = ""
-    
-    if msg.is_multipart():
-        for part in msg.walk():
-            if part.get_content_type() == "text/html":
-                email_body = part.get_payload(decode=True).decode()
-                break 
-    else:
-        email_body = msg.get_payload(decode=True).decode()
+with open(txt_path, "w", encoding="utf-8") as file:
+    for num in data[0].split():
+        result, msg_data = mail.fetch(num, "(RFC822)")
+        raw_email = msg_data[0][1]
+        msg = email.message_from_bytes(raw_email)
+        email_body = ""
 
-    soup = BeautifulSoup(email_body, "html.parser")
-    rows = soup.find_all("tr")
-    comercio, fecha, monto = None, None, None
+        if msg.is_multipart():
+            for part in msg.walk():
+                if part.get_content_type() == "text/html":
+                    email_body = part.get_payload(decode=True).decode()
+                    break 
+        else:
+            email_body = msg.get_payload(decode=True).decode()
 
-    for row in rows:
-        cells = row.find_all("td")
-        if len(cells) == 2:
-            key = cells[0].get_text(strip=True)
-            value = cells[1].get_text(strip=True)
+        soup = BeautifulSoup(email_body, "html.parser")
+        rows = soup.find_all("tr")
+        
+        comercio, fecha, monto = None, None, None
 
-            if "Comercio" in key:
-                comercio = value
-            elif "Fecha" in key:
-                fecha = value
-            elif "Monto" in key:
-                monto = value
-    
-    if comercio and fecha and monto:
-        transactions.append({
-            "fecha": fecha,
-            "comercio": comercio,
-            "monto": monto
-        })
-print(transactions)
+        for row in rows:
+            cells = row.find_all("td")
+            if len(cells) == 2:
+                key = cells[0].get_text(strip=True)
+                value = cells[1].get_text(strip=True)
 
-    
+                if "Comercio" in key:
+                    comercio = value
+                elif "Fecha" in key:
+                    fecha = value
+                elif "Monto" in key:
+                    monto = value
+
+        if comercio and fecha and monto:
+            file.write(f"Fecha: {fecha}\n")
+            file.write(f"Comercio: {comercio}\n")
+            file.write(f"Monto: {monto}\n")
+            file.write("-" * 40 + "\n")
+
 mail.logout()
